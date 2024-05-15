dcfbb099c4bfba3653e518ee7987aa2c8b478fde
Yago Ayala <yago.ayala.ius@gmail.com>
2024-05-15 17:34:26 -0300

    Se creó la carpeta de back, y se modificó config/boot/production

--- app/Views/Plantillas/login.php
+++ app/Views/Plantillas/login.php
@@ -14,13 +14,26 @@
                   <div class="d-flex flex-row align-items-center mb-4">
                     <i class="fas fa-user fa-lg me-3 fa-fw"></i>
                     <div class="form-outline flex-fill mb-0">
-                      <label class="form-label" for="form3Example1c">Nombre completo</label>
+                      <label class="form-label" for="form3Example1c">Nombre</label>
                       <input type="text" id="form3Example1c" class="form-control" required />
                       <div class="invalid-feedback">
-                        Por favor, ingresa tu nombre completo.
+                        Por favor, ingresa tu nombre.
                       </div>
                     </div>
                   </div>
+
+
+                  <div class="d-flex flex-row align-items-center mb-4">
+                    <i class="fas fa-user fa-lg me-3 fa-fw"></i>
+                    <div class="form-outline flex-fill mb-0">
+                      <label class="form-label" for="form3Example1c">Apellido</label>
+                      <input type="text" id="form3Example1c" class="form-control" required />
+                      <div class="invalid-feedback">
+                        Por favor, ingresa tu nombre apellido.
+                      </div>
+                    </div>
+                  </div>
+
 
                   <div class="d-flex flex-row align-items-center mb-4">
                     <i class="fas fa-envelope fa-lg me-3 fa-fw"></i>
