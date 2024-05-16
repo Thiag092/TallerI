20368e643054870d958a25ed108a1d3401e38e65
Yago Ayala <yago.ayala.ius@gmail.com>
2024-05-16 20:07:44 -0300

    Se realizo los puntos 2 y 3 de configuraciones de la base de datos. Se modifico dataBase.php y se creo usuario model.php. Además se soluciono las validaciones del formulario de Login

--- app/Models/usuario_model.php
+++ app/Models/usuario_model.php
@@ -6,5 +6,5 @@ class usuario_model extends Model                  /** la clase lleva siempre el
 {  
     protected $table = 'usuario';    /**Mismo nombre que la btabla que cree */
     protected $primaryKey = 'id_usuario';
-    protected $allowwedFields = ´'nombre', 'apellido', 'email', 'pass', 'perfil_id', 'baja','crated_at';
+    protected $allowwedFields = ´'nombre', 'apellido', 'email', 'pass', 'perfil_id', 'baja';
 }
--- app/Views/Plantillas/login.php
+++ app/Views/Plantillas/login.php
@@ -54,33 +54,19 @@
   </div>
 </section>
 
-
-<script>          //--------------- Ver por que no funciona del todo la validacion del formulario ---------------------
+<script>
   (function() {
     'use strict';
 
     window.addEventListener('load', function() {
-      // Fetch all the forms we want to apply custom Bootstrap validation styles to
       var forms = document.getElementsByClassName('needs-validation');
 
-      // Loop over them and prevent submission
       var validation = Array.prototype.filter.call(forms, function(form) {
         form.addEventListener('submit', function(event) {
-          var checkbox = form.querySelector('#gridCheck');
-
-          if (form.checkValidity() === false || !checkbox.checked) {
+          if (form.checkValidity() === false) {
             event.preventDefault();
             event.stopPropagation();
-
-            if (!checkbox.checked) {
-              checkbox.classList.add('is-invalid');
-            } else {
-              checkbox.classList.remove('is-invalid');
-            }
-          } else {
-            checkbox.classList.remove('is-invalid');
           }
-
           form.classList.add('was-validated');
         }, false);
       });
