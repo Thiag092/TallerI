cb0e6021e2e2281e082826dba00ed1c43dff6837
Yago Ayala <yago.ayala.ius@gmail.com>
2024-05-20 19:07:03 -0300

    Formulario Login
    se hicieorn cambios el formulario de inicio de sesión  para que tenga los campos de correo electrónico y contraseña y que esté configurado para enviar datos al servidor

--- app/Views/Plantillas/login.php
+++ app/Views/Plantillas/login.php
@@ -9,46 +9,37 @@
 
                 <p class="text-center h1 fw-bold mb-5 mx-1 mx-md-4 mt-4">Inicio de sesión</p>
 
-                <form class="mx-1 mx-md-4 needs-validation" novalidate>
+                <form class="mx-1 mx-md-4 needs-validation" action="<?php echo base_url('login'); ?>" method="post" novalidate>
+  <div class="d-flex flex-row align-items-center mb-4">
+    <i class="fas fa-user fa-lg me-3 fa-fw"></i>
+    <div class="form-outline flex-fill mb-0">
+      <label class="form-label" for="email">Correo</label>
+      <input type="text" id="email" name="email" class="form-control" placeholder="tuCorreo@Ejemplo.com" required />
+      <div class="invalid-feedback">
+        Por favor, ingresa tu correo electronico (ej. tuMail@gmail.com).
+      </div>
+    </div>
+  </div>
 
-                  <div class="d-flex flex-row align-items-center mb-4">
-                    <i class="fas fa-user fa-lg me-3 fa-fw"></i>
-                    <div class="form-outline flex-fill mb-0">
-                      <label class="form-label" for="form3Example1c">Correo</label>
-                      <input type="text" id="form3Example1c" class="form-control"  placeholder="tuCorreo@Ejemplo.com" required />
-                      <div class="invalid-feedback">
-                        Por favor, ingresa tu correo electronico (ej. tuMail@gmail.com).
-                      </div>
-                    </div>
-                  </div>
+  <div class="d-flex flex-row align-items-center mb-4">
+    <i class="fas fa-lock fa-lg me-3 fa-fw"></i>
+    <div class="form-outline flex-fill mb-0">
+      <label class="form-label" for="password">Contraseña</label>
+      <input type="password" id="pass" name="pass" class="form-control" placeholder="*******" required />
+      <div class="invalid-feedback">
+        Por favor, ingresa tu contraseña.
+      </div>
+    </div>
+  </div>
 
+  <div class="d-flex justify-content-center mx-4 mb-3 mb-lg-4 mt-4">
+    <button type="submit" class="btn btn-primary btn-lg btn-custom">Iniciar sesión</button>
+  </div>
+  <div class="RedireccionRegistro text-center">
+    <p> Todavía no te registraste? <a href="<?= base_url('registro') ?>" class="registro-link">Hacelo acá.</a></p>
+  </div>
+</form>
 
-
-
-                  <div class="d-flex flex-row align-items-center mb-4">
-                    <i class="fas fa-lock fa-lg me-3 fa-fw"></i>
-                    <div class="form-outline flex-fill mb-0">
-                      <label class="form-label" for="form3Example4c">Contraseña</label>
-                      <input type="password" id="form3Example4c" class="form-control" placeholder="*******" required />
-                      <div class="invalid-feedback">
-                        Por favor, ingresa tu contraseña.
-                      </div>
-                    </div>
-                  </div>
-
-                  
-
-                 
-
-              <div class="d-flex justify-content-center mx-4 mb-3 mb-lg-4 mt-4">
-                      <button type="submit" class="btn btn-primary btn-lg btn-custom">Iniciar sesión</button>
-                    </div>
-
-
-                    <div class="RedireccionRegistro text-center">
-                    <p> Todavía no te registraste? <a href="<?= base_url('registro') ?>" class="registro-link">Hacelo acá.</a></p>
-                  </div>
-                </form>
 
               </div>
             </div>
